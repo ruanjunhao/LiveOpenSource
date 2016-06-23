fc2fd46bc09b3117ba55537ff9feea60a96dcb50
LeeWongSnail <@wangli_0632@163.com>
2016-06-23 13:11:17 +0800

    avater

---
+++ LiveViewer/LiveViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveViewer
+//
+//  Created by LeeWong on 16/6/23.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveViewer/LiveViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveViewer
+//
+//  Created by LeeWong on 16/6/23.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
