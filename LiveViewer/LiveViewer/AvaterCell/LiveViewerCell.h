2ed1a533ed6742f7c9499d1f359cbf46d1acb0ba
LeeWongSnail <@wangli_0632@163.com>
2016-06-23 13:35:19 +0800

    add live

---
+++ LiveViewer/LiveViewer/AvaterCell/LiveViewerCell.h
@@ -0,0 +1,13 @@
+//
+//  LiveViewerCell.h
+//  LiveViewer
+//
+//  Created by LeeWong on 16/6/23.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiveViewerCell : UICollectionViewCell
+
+@end
---
+++ LiveViewer/LiveViewer/AvaterCell/LiveViewerCell.m
@@ -0,0 +1,31 @@
+//
+//  LiveViewerCell.m
+//  LiveViewer
+//
+//  Created by LeeWong on 16/6/23.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import "LiveViewerCell.h"
+
+@interface LiveViewerCell ()
+
+@property (nonatomic,strong) UIImageView *avaterIconImage;
+
+@end
+
+@implementation LiveViewerCell
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+//        self.avaterIconImage = [UIImageView alloc] initWithFrame:<#(CGRect)#>
+        
+        
+    }
+    
+    return self;
+}
+
+@end
